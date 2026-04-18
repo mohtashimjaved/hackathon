ff8047f6163434d9f9e465685734fa3fe6e71299
Mohtashim Javed <hafizmohtashim3157@gmail.com>
2026-04-19 04:21:47 +0500

    updated

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT
 
-app.use(cors());
 app.use(express.json());
+app.use(helmet())
+app.use(cors());
 
 mongoose.connection.on('open', () => {
   console.log('DB Connected');
