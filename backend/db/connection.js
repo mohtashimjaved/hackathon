f9d0c2fdc4a2f351ebe20c2540282f6a807a6b8b
Mohtashim Javed <hafizmohtashim3157@gmail.com>
2026-04-20 23:46:07 +0500

    Update connection.js

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -4,32 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const url = process.env.DB_URL;
-let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(url, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
-
-// Initial connect
-dbConnect();
+mongoose.connect(url)
 
 export default mongoose;
 
